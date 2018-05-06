5f23d0bdc3c95e084302f08b87346e294c2a1f81
d.hrynko <d.hrynko@godeltech.com>
2018-05-06 15:20:21 +0300

    Inicialized a new project

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+class Configurator
+{
+    const API_KEY = 'YOUR_API_KEY';
+    const REGION = 'YOUR_REGION';
+    const LANGUAGE = 'YOUR_LANGUAGE';
+    const DB_PATH = 'DB_PATH';
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: utscorpion
+ * Date: 5/6/18
+ * Time: 3:05 PM
+ */
+
